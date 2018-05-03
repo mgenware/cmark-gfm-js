6437e12630ede445e16fdc17ea028c51a9fba370
Mgen <m@mgenware.com>
2018-05-03 21:54:22 +0800

    Use pre.js to wrap the call to the internal _cmark_gfm_markdown_to_html

---
+++ pre.js
@@ -0,0 +1,18 @@
+var Module = {
+  convert: function(md, opt) {
+    if (typeof md !== 'string') {
+      throw new Error('Argument "md" must be an string.');
+    }
+
+    var len = lengthBytesUTF8(md) + 1;
+    var buffer = _malloc(len);
+    stringToUTF8(md, buffer, len);
+
+    var resBuffer = _cmark_gfm_markdown_to_html(buffer, len, opt);
+    var resString = UTF8ToString(resBuffer);
+
+    _free(buffer);
+    _free(resBuffer);
+    return resString;
+  }
+}
