fd761b557e9b6e431f4935474c1790a96e740301
Mgen <m@mgenware.com>
2018-05-03 19:16:41 +0800

    Use mkdir -p

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd ./lib/mgen-cmark-gfm
 make
 cd -
-mkdir dist
+mkdir -p dist
 emcc -O3 -o dist/libcmark.js ./lib/mgen-cmark-gfm/src/*.c ./lib/mgen-cmark-gfm/extensions/*.c -I ./lib/mgen-cmark-gfm/build/src -I ./lib/mgen-cmark-gfm/src -I ./lib/mgen-cmark-gfm/build/extensions -I ./lib/mgen-cmark-gfm/extensions -s EXPORTED_FUNCTIONS='["_cmark_gfm_markdown_to_html"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]'
